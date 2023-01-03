f883331d3d464a0d68f0808adde016d49fa6f07c
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-03 13:59:21 +0100

    Printing alphabet in lowercase using the ASCII except q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Printing alphabet in lowercase using the ASCII except q and e
+for letter in range(97, 123):
+    if 101 != letter != 113:
+        print(f"{letter:c}", end="")
