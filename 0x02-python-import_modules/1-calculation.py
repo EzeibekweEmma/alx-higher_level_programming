c0e3481adc43a1920e1205e6d25c3009c3486fa0
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-05 14:36:01 +0100

    program that imports maths functions from the file calculator_1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+# program that imports maths functions from the file calculator_1
+from calculator_1 import add, sub, div, mul
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
 
