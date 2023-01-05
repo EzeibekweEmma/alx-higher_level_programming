799bb96df498c7b9c3106d8cfb47d1c4237d48a1
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-05 13:42:39 +0100

    program that imports the function def add(a, b), from the file add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Program that imports the function def add(a, b), from the file add_0.py
-import add_0
+from add_0 import add
 
-a = 1
-b = 2
-
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+
+
