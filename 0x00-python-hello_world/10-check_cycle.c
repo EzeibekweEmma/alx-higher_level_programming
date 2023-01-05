4a4ea4666da629e4d4171873c40f9ae431039417
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-05 05:08:59 +0100

    function in C that checks if a singly linked list has a cycle in it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - function in C that checks if a singly linked list has a cycle
+ * in it
+ * @list: pointer
+ * Return: 0 or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
