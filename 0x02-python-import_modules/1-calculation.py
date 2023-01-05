fe39a3a9c3f502c1dff99bf2f49d362f280d2dd4
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-05 18:21:32 +0100

    added a description

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 # program that imports maths functions from the file calculator_1
-from calculator_1 import add, sub, div, mul
 
 if __name__ == "__main__":
+    """
+    program that imports maths functions from the file calculator_1
+    """
+    from calculator_1 import add, sub, div, mul
+
     a = 10
     b = 5
+
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
