7c0621ef9b68faefa5cbfc78298b580d4c066091
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-06 03:49:07 +0100

    program that imports the function def add(a, b): from the file add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Program that imports function def add(a, b), from the file add_0.py"""
+def sum():
     from add_0 import add
 
     a = 1
     b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    sum()
