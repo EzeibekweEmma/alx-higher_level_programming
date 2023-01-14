2ea67a9ed7a4503213c22557cfc55d2da80c5724
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-14 03:19:37 +0100

    rewrite code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 1:
-        for x in matrix:
-            for y in x:
-                print("{:d}".format(y), end=" ")
-            print(end="\n")
-    else:
+    for x in matrix:
+        s = ""
+        for y in x:
+            print("{:s}{:d}".format(s, y), end='')
+            s = " "
         print()
