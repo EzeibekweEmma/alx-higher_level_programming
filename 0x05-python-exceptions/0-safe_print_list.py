ac78ba7913a4ad037db8a315a4057ef620e91171
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-24 00:16:08 +0100

    fix two errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
+    except:
+            x = i
 
-        print(end="\n")
-        return i + 1
-    except IndexError:
-        print(end="\n")
-        return i
+    print(end="\n")
+    return x
