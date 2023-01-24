2b352dcf38e482f6208a644b74d6c54e326697ac
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-24 01:27:27 +0100

    function that prints an integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
     except IndexError:
-            x = i
+        x = i
+
     print(end="\n")
     return x
