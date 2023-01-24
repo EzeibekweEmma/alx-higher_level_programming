5382089233a16b578e485e4b3a711adfeb6f27fe
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-24 01:24:04 +0100

    fix bare 'except'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-    except:
+    except IndexError:
             x = i
-
     print(end="\n")
     return x
