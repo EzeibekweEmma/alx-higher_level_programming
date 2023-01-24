3e60965a2f5e6cdcf01cb65060982ae9a5dc6a18
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-01-24 01:47:21 +0100

    function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err))
+        result = None
+
+    return result
