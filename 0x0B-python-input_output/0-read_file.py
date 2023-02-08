2fd0509256bc89ed8a58cc281339c9319a1f51a1
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-02-08 05:52:01 +0100

    fix requirement

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     """
     reading a file
     """
-    with open(filename,  encoding="UTF-8") as a:
+    with open(filename, encoding="UTF-8") as a:
         print(a.read(), end="")
