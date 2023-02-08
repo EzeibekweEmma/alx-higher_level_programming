05057565d09ec24839cfb24fe468a654f37d2d66
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-02-08 19:57:39 +0100

    fix pycodestyle

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ def append_write(filename="", text=""):
     addending more texts to a file
     """
     with open(filename, "a", encoding="utf-8") as f:
-    return f.write(text)
+        return f.write(text)
