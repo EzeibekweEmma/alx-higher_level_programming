8524c7d91b2cf50aac25825c41baae65b471f910
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-02-08 05:50:46 +0100

    fix requirement

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Defines a file-writing function"""
+
 
 def write_file(filename="", text=""):
     '''
     writing to a file and returning the number of characters written
     '''
-    with open(filename, mode="w", encoding="UTF-8") as a:
+    with open(filename, "w", encoding="UTF-8") as a:
         return a.write(text)
