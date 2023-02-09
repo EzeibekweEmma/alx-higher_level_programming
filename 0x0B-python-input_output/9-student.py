ac8c41bee71aa059c36b88db5b08c672e0dd7168
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-02-09 23:49:50 +0100

    fix requirement fail

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Module"""
+
 
 class Student:
+    """
+    define a class student
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
