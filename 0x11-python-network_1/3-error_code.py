2f7a5332fbcb28cb03cbefc3a8fb06b3190e7656
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-02 10:47:56 +0100

    fix commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status"""
+"""Python script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8"""
 from urllib import error, request
 import sys
 
