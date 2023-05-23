70ba33b36efc7cbecd07cc038e1d1c8dd247b51d
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-23 12:06:12 +0100

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
+// Write a script that writes a string to a file.
 
 const fs = require('fs');
 const path = process.argv[2];
 const text = process.argv[3];
 
-const write = fs.writeFile(path, text, 'utf-8', (err) => {
+fs.writeFile(path, text, 'utf-8', (err) => {
   if (err) {
-    return err;
+    console.log(err);
   }
 });
-
-console.log(write);
