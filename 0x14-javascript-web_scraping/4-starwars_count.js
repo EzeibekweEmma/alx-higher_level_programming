c00b212c495d5803c43b56050b775a3358734957
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-23 12:21:55 +0100

    script that prints the number of movies where the character \“Wedge Antilles\” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,28 +1,25 @@
 #!/usr/bin/node
 // Write a script that prints the number of movies where the character
 // “Wedge Antilles” is present.
+const req = require('request');
+const args = require('process').argv;
+let count = 0;
 
-const request = require('request');
-const url = process.argv[2];
-const characterId = 18;
-
-request.get(url, (err, res, body) => {
-  if (err) console.log(err);
-  else {
-    const data = JSON.parse(body).results;
-
-    let count = 0;
-    data.forEach((film) => {
-      const characters = film.characters;
-      if (
-        characters.includes(
-          `https://swapi-api.alx-tools.com/api/people/${characterId}/`
-        )
-      ) {
-        count++;
+req.get(`${args[2]}`, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const result = JSON.parse(body).results;
+    for (let index = 0; index < result.length; index++) {
+      const item = result[index].characters;
+      for (let j = 0; j < item.length; j++) {
+        const val = item[j].split('/');
+        if (val[val.length - 2] === '18') {
+          count++;
+          break;
+        }
       }
-    });
-
-    console.log(count);
+    }
   }
+  console.log(count);
 });
