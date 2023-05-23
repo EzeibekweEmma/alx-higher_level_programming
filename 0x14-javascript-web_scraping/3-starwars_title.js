01513ef87d81f3079ff708369d15950c61f91d1d
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-23 12:18:42 +0100

    script that prints the title of a Star Wars movie where the episode number matches a given integer

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
-#!/usr/bin/Node
-
+#!/usr/bin/node
+//  script that prints the title of a Star Wars movie where the episode number
+//  matches a given integer.
+//
 const request = require('request');
-const id = process.argv[2];
 
-request.get(
-  {
-    url: `https://swapi-api.alx-tools.com/api/films/${id}`
-  },
-  (err, res, body) => {
-    console.log(err || JSON.parse(body).title);
-  }
-);
+const BASE_URL = 'https://swapi-api.hbtn.io/api/films/';
+const URL = BASE_URL + process.argv[2];
+
+request(URL, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.title);
+});
